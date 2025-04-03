199cbacc3784c0c64add1f6bcb3089ed8c1a821d
Taku Kitamura <takukitamura.io@gmail.com>
2025-04-03 09:38:16 +0900

    Update SQL comment

--- src/migrations/003-ctf.sql
+++ src/migrations/003-ctf.sql
@@ -6,6 +6,8 @@ CREATE TABLE Ctf (
 );
 
 INSERT INTO Ctf (id, title, template, attachmentButton) VALUES ('0', 'WebCTF', '', 'false');
+
+-- To add a bot, edit the following
 INSERT INTO Ctf (id, title, template, attachmentButton) VALUES ('1', 'Echo Bot', '', 'false');
 INSERT INTO Ctf (id, title, template, attachmentButton) VALUES ('2', 'アーティスト検索 Bot', 'search=santana<br>n=3', 'false');
 INSERT INTO Ctf (id, title, template, attachmentButton) VALUES ('3', 'グレースケール変換 Bot', '', 'true');
